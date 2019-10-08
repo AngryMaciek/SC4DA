ab36d269360186fc0fff3b06f7c4bc9be98d7e4b
Maciej Bak <wsciekly.maciek@gmail.com>
2019-10-08 19:34:54 +0200

    added test scripts [placeholders]

--- test.sh
+++ test.sh
@@ -2,15 +2,26 @@
 
 ###############################################################################
 #
-#   [TITLE, DESCRIPTION]
+#   Test script for the container build
 #
 #   AUTHOR: Maciej_Bak
 #   AFFILIATION: Swiss_Institute_of_Bioinformatics
 #   CONTACT: wsciekly.maciek@gmail.com
 #   CREATED: 06-10-2019
 #   LICENSE: GPL
-#   USAGE: ...
+#   USAGE: bash test.sh
 #
 ###############################################################################
 
-echo "Hello world!"
+echo "TESTING CONDA AND PYTHON:"
+echo $(conda --version)
+echo $(python --version)
+echo ""
+
+#echo "TESTING C AND C++ COMPILERS:"
+#echo $(gcc --version)
+#echo $(g++ --version)
+#echo ""
+
+echo "TESTING R:"
+echo $(R --version)
